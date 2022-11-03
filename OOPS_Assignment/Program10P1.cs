c1372d0841c3631ee6c272a0df82062c3c1d1a31
Jenil.Malkan <Jenil.Malkan@BLR-7W5GL13-L>
2022-11-03 15:13:39 +0530

    Add project files.

---
+++ OOPS_Assignment/Program10P1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS_Assignment
+{
+    class addition1
+    {
+        public int digit1, digit2;
+        public addition1()   //default contructor
+        {
+            digit1 = 100;
+            digit2 = 175;
+        }
+    }
+}
---
+++ OOPS_Assignment/Program7.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS_Assignment
+{
+    public class Parent
+    {
+        public void DisplayParentsAB()
+        {
+            Console.WriteLine("Ram and Sita are my parents");
+        }
+    }
+
+    public class Son : Parent
+    {
+        public void DisplaySonC()
+        {
+            Console.WriteLine("I am the son Jenil");
+        }
+    }
+}
